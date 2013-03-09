d5cec715ffe56d45bc901fd28ab639773ab501f7
root <root@DellWS.(none)>
2013-03-09 15:37:44 +0000

    fix make-freepto.sh

--- make-freepto.sh
+++ make-freepto.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
-read -p "Definisci il devices: " devicel
-read -p "Dimensione persistenza: " bytes
+set -x
+
+if [ $# != 1 ];then
+	echo "Wrong argument number"
+	exit 1
+fi
+device=$1
 
 # Check for bad block on the device:
-badblocks -c 10240 -s -w -t random -v "${devicel}"
+###badblocks -c 10240 -s -w -t random -v "${devicel}"
 
 # Random data on the device:
-dd if=/dev/urandom of="${devicel}"
+###dd if=/dev/urandom of="${devicel}"
 
 # DD THE binary.img to a usb
-dd if=binary.img of="${devicel}"
+#dd if=binary.img of="${device}"
 
 # Make the partition
-read bytes _ < <(du -bcm binary.img |tail -1); echo $bytes
+img_bytes=$(stat -c %s binary.img)
+img_bytes=$((img_bytes+1))
 
-parted "${devicel}" mkpart primary "${bytes}" "${usb_size}"
+parted "${device}" -- mkpart primary "${img_bytes}B" -1 
 
 # Ecnrypt partition
-cryptsetup --verbose --verify-passphrase luksFormat "${devicel}2"
+cryptsetup --verbose --batch-mode luksFormat "${device}2" <<<freepto
 
 # Open partition
-cryptsetup luksOpen "${devicel}2" my_usb
+cryptsetup luksOpen "${device}2" my_usb <<<freepto
 
 # Make FS with label: "persistence"
-mkfs.ext3 -L persistence /dev/mapper/my_usb
+mkfs.btrfs -L persistence /dev/mapper/my_usb
 
 # Make a mount point
 mkdir -p /mnt/my_usb
 
 # Munt the partition
-mount /dev/mapper/my_usb /mnt/my_usb/
+mount /dev/mapper/my_usb /mnt/my_usb/ -o noatime,nodiratime,compress=lzo
 
 # Make the persistence.conf file
-echo "/ union" > ~/persistence.conf && mv ~/persistence.conf \
-                      /persistence.conf && mv /persistence.conf /mnt/my_usb
+echo "/ union" > ~/persistence.conf 
+mv ~/persistence.conf /persistence.conf 
+mv /persistence.conf /mnt/my_usb
 
 # Umount
 umount /dev/mapper/my_usb
