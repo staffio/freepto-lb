bc54a98ddad95df82b573f87ba3f1f27384ad2bf
root <root@DellWS.(none)>
2013-03-14 20:37:07 +0000

    fix script

--- make-freepto.sh
+++ make-freepto.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 set -x
 
+# Check if user is root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 if [ $# != 1 ];then
-	echo "Wrong argument number"
-	exit 1
+   echo "Wrong argument number"
+   exit 1
 fi
 device=$1
 
 # Check for bad block on the device:
-###badblocks -c 10240 -s -w -t random -v "${devicel}"
+badblocks -c 10240 -s -w -t random -v "${devicel}"
 
 # Random data on the device:
-###dd if=/dev/urandom of="${devicel}"
+dd if=/dev/urandom of="${devicel}"
 
 # DD THE binary.img to a usb
-#dd if=binary.img of="${device}"
+dd if=binary.img of="${device}"
 
 # Make the partition
 img_bytes=$(stat -c %s binary.img)
 img_bytes=$((img_bytes+1))
 
-parted "${device}" -- mkpart primary "${img_bytes}B" -1 
+parted "${device}" -- mkpart primary "${img_bytes}B" -1
 
 # Ecnrypt partition
 cryptsetup --verbose --batch-mode luksFormat "${device}2" <<<freepto
@@ -38,8 +44,8 @@ mkdir -p /mnt/my_usb
 mount /dev/mapper/my_usb /mnt/my_usb/ -o noatime,nodiratime,compress=lzo
 
 # Make the persistence.conf file
-echo "/ union" > ~/persistence.conf 
-mv ~/persistence.conf /persistence.conf 
+echo "/ union" > ~/persistence.conf
+mv ~/persistence.conf /persistence.conf
 mv /persistence.conf /mnt/my_usb
 
 # Umount
@@ -47,3 +53,4 @@ umount /dev/mapper/my_usb
 
 # Close LUKS
 cryptsetup luksClose /dev/mapper/my_usb
+
